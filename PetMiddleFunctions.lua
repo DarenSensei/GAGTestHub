a4a286797ebf6f415e99c069ef909ee926c0eac7
Darensensei <113220972+DarenSensei@users.noreply.github.com>
2025-07-22 14:22:40 +0800

    Update PetMiddleFunctions.lua

--- PetMiddleFunctions.lua
+++ PetMiddleFunctions.lua
@@ -10,8 +10,8 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
 
 -- Pet Radius Control Configuration
-local RADIUS = 0.1
-local LOOP_DELAY = 1
+local RADIUS = 0.5
+local LOOP_DELAY = 3
 local INITIAL_LOOP_TIME = 5
 local ZONE_ABILITY_DELAY = 3
 local ZONE_ABILITY_LOOP_TIME = 3
